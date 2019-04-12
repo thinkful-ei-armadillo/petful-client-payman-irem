56b5bd96968420eb0ba02c37e8243bf2542185dc
Irem Sen <secilreel@yahoo.com>
2019-04-12 13:58:04 -0400

    working on adoption page

--- src/Adoption.js
+++ src/Adoption.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import PetListContext from "./PetListContext";
 
 export default class Adoption extends Component {
@@ -10,26 +9,25 @@ export default class Adoption extends Component {
     const dog = this.props.dogs;
     return (
     <div>
-    <div className="cat container">
-        <h2>{cat.title}</h2>
-        <div className="cat-box">
-            <img src={cat.image} className="cat-owner logo" alt="headshot of the cat planner" />
-            <p>{cat.name}</p>
+        <div className="cat container">
+            <h2>{cat.name}</h2>
+            <div className="cat-box">
+                <img src={cat.image} className="cat image" alt="a close shot of the cat" />
+                <p>{cat.name}</p>
+            </div>
+            <div className="button-box">
+                <button type="button" onClick={this.handleClickAdoptButton}>Adopt Me!</button>
+            </div>      
         </div>
-        <div className="button-box">
-            <button type="button" onClick={this.handleClickAdoptButton}> Me!</button>
-        </div>      
-    </div>
-    <div className="dog container">
-    <h2>{cat.title}</h2>
-    <div className="cat-box">
-        <img src={cat.image} className="cat-owner logo" alt="headshot of the cat planner" />
-        <p>{cat.name}</p>
-    </div>
-    <div className="button-box">
-        <button type="button" onClick={this.handleClickAdoptButton}> Me!</button>
-    </div>      
-    </div> 
+        <div className="dog container">
+            <h2>{dog.name}</h2>
+            <div className="dog-box">
+                <img src={dog.image} className="cat-owner logo" alt="a close shot of the dog" />
+            </div>
+            <div className="button-box">
+                <button type="button" onClick={this.handleClickAdoptButton}>Adopt Me!</button>
+            </div>      
+        </div> 
     </div>
     )  
   }
