282cc8ad3a9766818cb0b50a196c4ac8b3f60711
Irem Sen <secilreel@yahoo.com>
2019-04-13 22:01:31 -0400

    updated the homepage with description and image, prepared for deployment

--- src/App.js
+++ src/App.js
@@ -18,24 +18,20 @@ class App extends Component {
         <header className="App-header">
           {this.state.view === 'home' && (
             <React.Fragment>
-              <div>
+              <div className ='welcome'>
                 <h1>
-                  Adopt a FIFO pet; give an equal opportunity to a pet in
-                  shelter!
+                  Adopt a FIFO pet; give a shelter pet an equal opportunity in!
                 </h1>
                 <p>
-                  Welcome to Adopt a Pet FIFO style page. We're an animal
-                  shelter that allows adoption of cats and dogs only in the
-                  order they join us. We only have two types of pets: cats and
-                  dogs, all of which wait for you to take your home. The
-                  adoption process works strictly on a "First-In, First-Out"
-                  basis. The FIFO is based on the animals that came to the
-                  shelter first. You can adopt a cat, or a dog, or both. The
-                  only rule is that we ask you to adopt the animal that came to
-                  the shelter first to give an equal opportunity to all of our
-                  animals If you'd like to start the process, get in queue, just
-                  like our pets, so you can adopt when its your turn.
+                  Welcome to Adopt a Pet FIFO style page.
+                  We're an animal shelter that allows adoption of cats and dogs only in the order they join us. 
+                  We only have two types of pets: cats and dogs, all of which wait for you to take them to your home. 
+                  The adoption process works strictly on a "First-In, First-Out" basis. 
+                  You can adopt a cat, or a dog, or both. 
+                  The only rule is that we ask you to adopt the animal that came to our shelter first.
+                  If you'd like to start the process, get in the queue, just like our pets, so you can adopt when its your turn.
                 </p>
+                <img className='dogque' src='http://www.nedmartin.org/v3/amused/_img/dog-queue.jpg' alt='dogs waiting in line'/>
               </div>
               <button onClick={this.changeView}>Start</button>
             </React.Fragment>
